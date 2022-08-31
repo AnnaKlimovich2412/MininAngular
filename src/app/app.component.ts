d8f21316e5c106f46b05dc6628520b367197df2d
annaklimovich <klimovichannet@gmail.com>
2022-08-31 12:40:25 +0200

    feat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MininAngular';
+  posts: Post[] = [
+    {title: 'First', text: 'I love my home', id: 1},
+    {title: 'Second', text: 'I love Vova', id: 2}
+  ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+  }
 }
